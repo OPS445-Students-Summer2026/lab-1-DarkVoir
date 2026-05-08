9cad7c29e6bec1ae7b745ecd584a9b8016104b6d
Krishna Lamichhane <klamichhane1@myseneca.ca>
2026-05-08 18:11:21 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Creator: Krishna Lamichhane'''
+'''Date: May 8, 2026'''
+'''Course: OPS445 NAA'''
+
+'''Giving value to the object'''
+x = 10
+y = 2
+z = 5
+
+'''Displaying the result'''
+print(f"{x} + {y} * {z} = {x + y * z}")
