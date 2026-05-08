9fcd8ecddb6935ad1c6c324d3fa5d09b03c28a8f
Krishna Lamichhane <klamichhane1@myseneca.ca>
2026-05-08 14:16:59 -0400

    Added lab1a

---
+++ lab1a.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Created by: Krishna Lamichhane'''
+'''Email: klamichhane1@myseneca.ca'''
+'''Course: OPS445 NAA'''
+'''Description: This program will output "hello world" to the screen'''
+
+print('Hello world')
