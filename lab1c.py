19d75f42e610da6e0e93227b4636355741997981
Krishna Lamichhane <klamichhane1@myseneca.ca>
2026-05-08 14:42:23 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Created by: Krishna Lamichhane'''
+'''Date: May 8, 2026'''
+'''Course: OPS445 NAA'''
+
+'''An object to containt the values'''
+name = 'Isaac' 
+age = 72
+
+
+'''Displaying the final result'''
+print(name + ' is ' + str(age) + ' years old!')
